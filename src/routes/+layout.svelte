9cfd4c44a0cc9d773f54774e5a180bf32e9459c8
Jan Podmolik <jan.podmolik@gmail.com>
2025-11-17 17:54:55 +0100

    Fix build configuration for custom domain

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,6 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href="/favicon.ico" />
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="author" content="Jan Podmolík" />
